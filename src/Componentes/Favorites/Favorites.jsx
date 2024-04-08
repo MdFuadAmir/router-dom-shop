d53e6b4bc54e56e5c5f1da1c94380ff1d5990ed8
Md Fuad Amir <mdfuadamir@gmail.com>
2024-04-09 04:54:15 +0600

    added favorite section start

--- src/Componentes/Favorites/Favorites.jsx
+++ src/Componentes/Favorites/Favorites.jsx
@@ -3,7 +3,24 @@
 const Favorites = () => {
     return (
         <div>
-            favorites
+            <div className='my-10'>
+            <div className='w-full h-[30vh] mb-6 bg-lime-200'>
+                <h2 className='mx-auto w-fit text-4xl'>Phone Details</h2>
+            </div>
+
+            <div className="card card-side bg-base-100 shadow-xl w-2/3 mx-auto">
+  <figure><img className='h-[60vh]' src='ds' alt="Movie"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Name: fgf</h2>
+    <h4>Brand: fgf</h4>
+    <h4>Price: fgf</h4>
+    <h4>Rating: fgf</h4>
+    <div className="card-actions justify-end">
+      <button className="btn btn-outline w-full">add to Favorite</button>
+    </div>
+  </div>
+</div>
+        </div>
         </div>
     );
 };
