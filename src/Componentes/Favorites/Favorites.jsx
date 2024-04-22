1d2f0dc4a6f83f5407a1cfa4c7ac24da3b02d167
Md Fuad Amir <mdfuadamir@gmail.com>
2024-04-22 21:47:19 +0600

    added afgfg

--- src/Componentes/Favorites/Favorites.jsx
+++ src/Componentes/Favorites/Favorites.jsx
@@ -1,27 +1,31 @@
+import { useEffect, useState } from "react";
+import PhoneCard from "../PhoneCard/PhoneCard";
 
 
 const Favorites = () => {
+  const [favorites, setfavorites] = useState([]);
+  const [noFound, setNoFound] = useState(false);
+
+  useEffect(()=>{
+    const favoriteItems = JSON.parse(localStorage.getItem('test'));
+    if(favoriteItems){
+      setfavorites(favoriteItems);
+    }
+    else{
+      setNoFound("No Data Found");
+    }
+  },[])
+  
     return (
         <div>
-          
-            {/* <div className='my-10'>
-            <div className='w-full h-[30vh] mb-6 bg-lime-200'>
-                <h2 className='mx-auto w-fit text-4xl'>Phone Details</h2>
-            </div>
+          {
+            noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> : <div>
+              {
+                favorites.map(phone => <PhoneCard key={phone.id} phone={phone}></PhoneCard>)
+              }
 
-            <div className="card card-side bg-base-100 shadow-xl w-2/3 mx-auto">
-  <figure><img className='h-[60vh]' src='ds' alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">Name: fgf</h2>
-    <h4>Brand: fgf</h4>
-    <h4>Price: fgf</h4>
-    <h4>Rating: fgf</h4>
-    <div className="card-actions justify-end">
-      <button className="btn btn-outline w-full">Add to Favorite</button>
-    </div>
-  </div>
-</div>
-        </div> */}
+            </div>
+          } 
         </div>
     );
 };
